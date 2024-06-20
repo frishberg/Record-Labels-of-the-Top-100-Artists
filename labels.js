6082d354febf908b775c400b74426fef8d3a0844
frishberg <frishberg@uchicago.edu>
2024-06-20 05:24:13 +0000

    daily update of labels.json

--- labels.js
+++ labels.js
@@ -336,7 +336,8 @@ var data = {
         "Alamo"
     ],
     "Jessie Murph": [
-        "Disruptor Records"
+        "Disruptor",
+        "Columbia"
     ],
     "Djo": [
         "AWAL"
@@ -467,4 +468,4 @@ var data = {
         "Capitol",
         "Virgin"
     ]
-};var lastUpdated = "06/19";
+};var lastUpdated = "06/20";
